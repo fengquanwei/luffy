28a176c3a9ca9a6263cba24c6d61d072be40fcc2
fengquanwei <fengquanwei1996@163.com>
2018-10-29 23:50:45 +0800

    项目初始化

---
+++ src/main/java/com/fengquanwei/luffy/LuffyApplication.java
@@ -0,0 +1,12 @@
+package com.fengquanwei.luffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuffyApplication.class, args);
+	}
+}
